c679c6379659fbf05b73099081c199f7088d51cb
dphuang2 <dylan@konfigthis.com>
2024-08-15 19:41:07 -0700

    Revert "okay, remove all nextjs backend stuff"
    This reverts commit f87ac17c1cbb3aa52012f83c31bda84211777e48.

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+
+function generateRandomString(length: number) {
+  var text = "";
+  var possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+export async function GET(req: Request) {
+  var scope =
+    "streaming \
+               user-read-email \
+               user-read-private";
+
+  var state = generateRandomString(16);
+
+  let spotify_client_id = process.env.SPOTIFY_CLIENT_ID;
+
+  if (spotify_client_id === undefined)
+    throw Error("Missing SPOTIFY_CLIENT_ID environment variable");
+
+  var auth_query_parameters = new URLSearchParams({
+    response_type: "code",
+    client_id: spotify_client_id,
+    scope: scope,
+    redirect_uri: "http://localhost:3000/api/auth/callback",
+    state: state,
+  });
+
+  return NextResponse.redirect(
+    "https://accounts.spotify.com/authorize/?" +
+      auth_query_parameters.toString()
+  );
+}
