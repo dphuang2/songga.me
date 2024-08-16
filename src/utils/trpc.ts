817c88157fc99f545bd143e5217a9b3af3caa2d7
dphuang2 <dylan@konfigthis.com>
2024-08-15 20:54:47 -0700

    restart, lets use FastAPI

--- src/utils/trpc.ts
+++
@@ -1,9 +0,0 @@
-import { createTRPCReact } from "@trpc/react-query";
-import { appRouter, type AppRouter } from "@/app/server/routers/_app";
-import { createCallerFactory } from "@/app/server/trpc";
-
-export const trpc = createTRPCReact<AppRouter>();
-const callerFactory = createCallerFactory(appRouter);
-export const caller = callerFactory({
-  foo: "bar",
-});
