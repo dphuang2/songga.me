b5171d52cc15d2d201958851006a8cff6e109883
dphuang2 <dylan@konfigthis.com>
2024-09-18 19:31:07 -0700

    fix nextjx hydration error

--- components/StartAGame.tsx
+++ components/StartAGame.tsx
@@ -4,9 +4,18 @@ import { User } from "@supabase/auth-js";
 import { CreateAGameButton } from "./CreateAGameButton";
 import { SignInWithSpotifyButton } from "./SignInWithSpotifyButton";
 import { SpotifyAuthStorage } from "@/utils/supabase/client";
+import { AccessToken } from "@spotify/web-api-ts-sdk";
+import { useState, useEffect } from "react";
 
 export function StartAGame({ user }: { user: User | null }) {
-  const savedSpotifyAccessToken = SpotifyAuthStorage.getSavedAccessToken();
+  const [savedSpotifyAccessToken, setSavedSpotifyAccessToken] =
+    useState<AccessToken | null>(null);
+
+  useEffect(() => {
+    const token = SpotifyAuthStorage.getSavedAccessToken();
+    setSavedSpotifyAccessToken(token);
+  }, []);
+
   return (
     <>
       {user && savedSpotifyAccessToken !== null ? (
